ec52baf7ca5553c150eb81e97b2473c2e6ab22c6
Akashris <akashritharan@gmail.com>
2019-09-30 13:57:59 +0530

    Task1

---
+++ sum in a triang.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            int r, i, j;
+            int[,] a = new int[10, 10];
+            Console.WriteLine("Enter no. of rows");
+            r = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter The Values");
+            for (i = 0; i < r; i++)
+            {
+                for (j = 0; j <= i; j++)
+                {
+                    a[i,j]= int.Parse(Console.ReadLine());
+                    //Console.WriteLine(data);
+                    //a[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (i = 0; i < r; i++)
+            {
+                for (j = 0; j <= i; j++)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine(a[i,j]);
+                    //a[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (i = r - 2; i >= 0; i--)
+            {
+                for (j = 0; j <= i; j++)
+                {
+                    if (a[i + 1, j] > a[i + 1, j + 1])
+                    {
+                        a[i, j] += a[i + 1, j];
+                    }
+                    else
+                    {
+                        a[i, j] += a[i + 1, j + 1];
+                    }
+                }
+            }
+            Console.Write("{0}",a[0,0]);
+            Console.ReadLine();
+        }
+    }
+}
